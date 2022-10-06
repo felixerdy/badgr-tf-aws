d17d768ac191a78f716418f5af6ef5ca9026c115
Felix Erdmann <hello@felixerdmann.com>
2022-10-06 16:07:45 +0200

    add s3 policy

--- iam/main.tf
+++ iam/main.tf
@@ -20,11 +20,34 @@ resource "aws_iam_policy" "ses_full_access" {
             Resource: "*"
         }
     ]
-})
+  })
 }
 
-resource "aws_iam_policy_attachment" "attach_policy" {
+resource "aws_iam_policy" "s3_full_access" {
+  name        = "s3_full_access"
+
+  policy = jsonencode({
+    Version: "2012-10-17",
+    Statement: [
+        {
+            Effect: "Allow",
+            Action: [
+                "s3:*"
+            ],
+            Resource: "*"
+        }
+    ]
+  })
+}
+
+resource "aws_iam_policy_attachment" "attach_ses_policy" {
   name       = "ses_policy_attachment"
   users      = [aws_iam_user.user.name]
   policy_arn = aws_iam_policy.ses_full_access.arn
+}
+
+resource "aws_iam_policy_attachment" "attach_s3_policy" {
+  name       = "s3_policy_attachment"
+  users      = [aws_iam_user.user.name]
+  policy_arn = aws_iam_policy.s3_full_access.arn
 }
